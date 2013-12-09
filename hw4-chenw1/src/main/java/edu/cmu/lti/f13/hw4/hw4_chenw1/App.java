00100947596a8d04fd193fb00b3a41b2e7b6adf7
yicaoyimu <yicaoyimu.wky@gmail.com>
2013-12-09 18:32:54 -0500

    initial checkin

---
+++ hw4-chenw1/src/main/java/edu/cmu/lti/f13/hw4/hw4_chenw1/App.java
@@ -0,0 +1,13 @@
+package edu.cmu.lti.f13.hw4.hw4_chenw1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
